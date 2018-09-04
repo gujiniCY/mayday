75165ef2e68bcba79bf2cc15ef7df459ca20316a
songhaozhi <1150837340@qq.com>
2018-09-04 16:52:48 +0800

    初始提交

--- src/main/java/com/bukaopu/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.bukaopu.service;
-/**
-* @author 作者:宋浩志
-* @createDate 创建时间：2018年8月27日 上午11:14:15
-*/
-public interface UserService {
-
-}
---
+++ src/main/java/com/songhaozhi/mayday/service/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.songhaozhi.mayday.service.impl;
+/**
+* @author 作者:宋浩志
+* @createDate 创建时间：2018年8月27日 上午11:14:35
+*/
+
+import com.songhaozhi.mayday.service.UserService;
+
+public class UserServiceImpl implements UserService{
+
+}
