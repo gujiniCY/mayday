1a2fac3096983bb3155ac810e4faf34c2bf5053e
songhaozhi <1150837340@qq.com>
2018-09-04 16:56:01 +0800

    .

--- src/test/java/com/songhaozhi/mayday/ApplicationTests.java
+++ src/test/java/com/songhaozhi/mayday/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bukaopu.bukaopu;
+package com.songhaozhi.mayday;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BukaopuApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
