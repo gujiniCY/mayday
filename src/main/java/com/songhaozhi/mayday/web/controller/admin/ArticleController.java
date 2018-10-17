a8d22a8ecbc5ccb50e98bf4a532dc6249f9dbf87
songhaozhi <1150837340@qq.com>
2018-10-17 16:20:15 +0800

    写了啥呢

--- src/main/java/com/songhaozhi/mayday/web/controller/admin/ArticleController.java
+++ src/main/java/com/songhaozhi/mayday/web/controller/admin/ArticleController.java
@@ -1,8 +1,14 @@
 package com.songhaozhi.mayday.web.controller.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.songhaozhi.mayday.model.domain.Article;
+import com.songhaozhi.mayday.service.ArticleService;
 
 /**
 * @author 宋浩志
@@ -12,10 +18,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin/article")
 public class ArticleController extends BaseController{
-	
+	@Autowired
+	private ArticleService articleService;
 	@GetMapping
-	public String article() {
+	public String article(Article article , @RequestParam(value="page",defaultValue="1") int page,@RequestParam(value="limit",defaultValue="10") int limit, Model model) {
 		return "/admin/admin_article";
+	}
+	@GetMapping(value="/new")
+	public String newArticle() {
+		return "/admin/admin_new_article";
 	}
 
 }
