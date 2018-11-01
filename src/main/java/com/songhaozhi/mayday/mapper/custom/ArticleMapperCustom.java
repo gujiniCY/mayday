86622408a9b0ac4c1bef77d8061eeb3c024254d7
songhaozhi <1150837340@qq.com>
2018-11-01 17:01:53 +0800

    写了些bug

---
+++ src/main/java/com/songhaozhi/mayday/mapper/custom/ArticleMapperCustom.java
@@ -0,0 +1,15 @@
+package com.songhaozhi.mayday.mapper.custom;
+
+import java.util.List;
+
+import com.songhaozhi.mayday.model.domain.ArticleCustom;
+
+/**
+* @author 作者:宋浩志
+* @createDate 创建时间：2018年11月1日 下午2:54:48
+*/
+public interface ArticleMapperCustom {
+
+	List<ArticleCustom> articleMapperCustom();
+
+}
