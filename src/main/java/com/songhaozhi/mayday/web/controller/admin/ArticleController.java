6797e4e1e6adacb173c4a9ef74e767fc38b9e6d9
songhaozhi <songhaozhi@DESKTOP-UDKOSKS>
2018-11-25 22:46:33 +0800

    测试

--- src/main/java/com/songhaozhi/mayday/web/controller/admin/ArticleController.java
+++ src/main/java/com/songhaozhi/mayday/web/controller/admin/ArticleController.java
@@ -47,8 +47,10 @@ public class ArticleController extends BaseController {
 	private TagService tagService;
 	@Autowired
 	private ArticleService articleService;
+
 	/**
 	 * 显示所有文章
+	 * 
 	 * @param model
 	 * @param page
 	 * @param limit
@@ -56,14 +58,15 @@ public class ArticleController extends BaseController {
 	 */
 	@GetMapping
 	public String article(Model model, @RequestParam(value = "page", defaultValue = "1") int page,
-			@RequestParam(value = "limit", defaultValue = "10") int limit,@RequestParam(value="status",defaultValue="0") int status) {
-		PageInfo<ArticleCustom> pageInfo = articleService.findPageArticle(page, limit,status);
-		//已发布条数
-		Integer published=articleService.countByStatus(0);
-		//草稿条数
-		Integer draft=articleService.countByStatus(1);
-		//回收站条数
-		Integer recycle=articleService.countByStatus(2); 
+			@RequestParam(value = "limit", defaultValue = "10") int limit,
+			@RequestParam(value = "status", defaultValue = "0") int status) {
+		PageInfo<ArticleCustom> pageInfo = articleService.findPageArticle(page, limit, status);
+		// 已发布条数
+		Integer published = articleService.countByStatus(0);
+		// 草稿条数
+		Integer draft = articleService.countByStatus(1);
+		// 回收站条数
+		Integer recycle = articleService.countByStatus(2);
 		model.addAttribute("info", pageInfo);
 		model.addAttribute("draft", draft);
 		model.addAttribute("recycle", recycle);
@@ -99,10 +102,10 @@ public class ArticleController extends BaseController {
 	@ResponseBody
 	public JsonResult save(Article article, Long[] tags, Long[] categorys, HttpServletRequest request) {
 		try {
-			if(StrUtil.isEmpty(article.getArticleTitle())) {
+			if (StrUtil.isEmpty(article.getArticleTitle())) {
 				return new JsonResult(false, "文章标题不能为空");
 			}
-			if(StrUtil.isEmpty(article.getArticleUrl())) {
+			if (StrUtil.isEmpty(article.getArticleUrl())) {
 				return new JsonResult(false, "文章链接不能为空");
 			}
 			User user = (User) request.getSession().getAttribute(MaydayConst.USER_SESSION_KEY);
@@ -161,27 +164,31 @@ public class ArticleController extends BaseController {
 		}
 		return new JsonResult(MaydayEnums.OPERATION_SUCCESS.isFlag(), MaydayEnums.OPERATION_SUCCESS.getMessage());
 	}
+
 	/**
 	 * 修改文章状态为回收站
+	 * 
 	 * @param id
 	 * @return
 	 */
-	@GetMapping(value="/recycle")
-	public String updateStatus(@RequestParam(value="id") int id) {
+	@GetMapping(value = "/recycle")
+	public String updateStatus(@RequestParam(value = "id") int id) {
 		try {
-			articleService.recycle(id,ArticleStatus.RECYCLE.getStatus());
+			articleService.recycle(id, ArticleStatus.RECYCLE.getStatus());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return "redirect:/admin/article";
 	}
+
 	/**
 	 * 彻底删除文章
+	 * 
 	 * @param id
 	 * @return
 	 */
-	@GetMapping(value="/remove")
-	public String remove(@RequestParam(value="id") int id) {
+	@GetMapping(value = "/remove")
+	public String remove(@RequestParam(value = "id") int id) {
 		try {
 			articleService.remove(id);
 		} catch (Exception e) {
@@ -204,41 +211,44 @@ public class ArticleController extends BaseController {
 		model.addAttribute("tags", tags);
 		return "/admin/admin_new_article";
 	}
+
 	/**
 	 * 修改文章页面
+	 * 
 	 * @param model
 	 * @return
 	 */
-	@GetMapping(value="/edit")
-	public String editArticle(Model model,@RequestParam(value="article_id")Integer article_id) {
-		//获取所有分类
+	@GetMapping(value = "/edit")
+	public String editArticle(Model model, @RequestParam(value = "article_id") Integer article_id) {
+		// 获取所有分类
 		List<Category> categorys = categoryService.findCategory();
-		//获取所有标签
+		// 获取所有标签
 		List<Tag> tags = tagService.findTags();
-		//获取文章信息
-		ArticleCustom articleCustom=articleService.findByArticleId(article_id);
+		// 获取文章信息
+		ArticleCustom articleCustom = articleService.findByArticleId(article_id);
 		model.addAttribute("categorys", categorys);
 		model.addAttribute("tags", tags);
 		model.addAttribute("articleCustom", articleCustom);
 		return "/admin/admin_edit_article";
 	}
+
 	/**
 	 * 
-	 * @param article_id 文章id
+	 * @param article_id
+	 *            文章id
 	 * @return 该篇文章关联的分类和标签
 	 */
-	@PostMapping(value="/ids") 
+	@PostMapping(value = "/ids")
 	@ResponseBody
 	public Map<String, Object> ids(Integer article_id) {
-		Map<String, Object> map=new HashMap<String, Object>();
-		//获取文章信息
-		ArticleCustom articleCustom=articleService.findByArticleId(article_id);
-		String[] tags=articleCustom.getTags().split(",");
+		Map<String, Object> map = new HashMap<String, Object>();
+		// 获取文章信息
+		ArticleCustom articleCustom = articleService.findByArticleId(article_id);
+		String[] tags = articleCustom.getTags().split(",");
 		String[] categorys = articleCustom.getCategorys().split(",");
 		map.put("tagsIds", tags);
 		map.put("categorysIds", categorys);
 		return map;
 	}
-	
-	
+
 }
