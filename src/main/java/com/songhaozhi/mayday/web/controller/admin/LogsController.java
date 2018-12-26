88a6a344185f579b43c6879452fadf97ba4e89d9
songhaozhi <1150837340@qq.com>
2018-12-26 15:51:31 +0800

    查看日志

---
+++ src/main/java/com/songhaozhi/mayday/web/controller/admin/LogsController.java
@@ -0,0 +1,31 @@
+package com.songhaozhi.mayday.web.controller.admin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.github.pagehelper.PageInfo;
+import com.songhaozhi.mayday.model.domain.Log;
+import com.songhaozhi.mayday.service.LogService;
+
+/**
+* @author : 宋浩志
+* @createDate : 2018年12月26日
+*/
+@Controller
+@RequestMapping("/admin/logs")
+public class LogsController extends BaseController{
+	@Autowired
+	private LogService logService;
+	@GetMapping
+	public String logs(Model model,@RequestParam(value = "page", defaultValue = "1") int page,
+			@RequestParam(value = "limit", defaultValue = "10") int limit) {
+		PageInfo<Log> info=logService.findLogs(page, limit);
+		model.addAttribute("logs", info);
+		return "/admin/view_logs";
+	}
+
+}
